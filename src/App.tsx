aee71b1a00bccdb7aac831aeb903b960f54e2c6b
Phil Gates-Shannon <phil@contactually.com>
2019-01-03 09:06:23 -0500

    Add Font Awesome packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-// import './global/icons';
+import './Global/icons';
 import './stylesheets/app.sass';
 
-// TODO Add Bootstrap and FontAwesome
 // TODO Add production build configuration
 class App extends React.Component {
   render() {
     return (
       <main role="main" className="container py-4">
-        <div className="starter-template">
-          <h1>Bootstrap grid examples</h1>
-          <p className="lead">
-            Basic grid layouts to get you familiar with building within the Bootstrap grid system.
-          </p>
-          <div className="row mb-3">
-            <div className="col-4">.col-4</div>
-            <div className="col-4">.col-4</div>
-            <div className="col-4">.col-4</div>
+        <h1>Bootstrap grid examples</h1>
+        <p className="lead">
+          Basic grid layouts to get you familiar with building within the Bootstrap grid system.
+        </p>
+        <div className="row mb-3">
+          <div className="col-4">
+            Light coffee: <FontAwesomeIcon icon={['fal', 'coffee']} />
+          </div>
+          <div className="col-4">
+            Regular coffee: <FontAwesomeIcon icon={['far', 'coffee']} />
+          </div>
+          <div className="col-4">
+            Solid coffee: <FontAwesomeIcon icon={['fas', 'coffee']} />
+          </div>
+          <div className="col-4">
+            Twitter: <FontAwesomeIcon icon={['fab', 'twitter']} />
           </div>
         </div>
       </main>
